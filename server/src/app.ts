552cafecc639650df15bc526c43329c73ff8811b
Dev-x2 <john@doe.org>
2021-07-03 17:46:10 +0300

    Add port

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,12 +2,12 @@ import express from 'express';
 import path from 'path';
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 const clientAppLocation = path.join(__dirname, '../public');
 
 app.use(express.static(clientAppLocation));
 app.use('*', express.static(clientAppLocation));
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`App is running at http://localhost:${port}`);
 });
